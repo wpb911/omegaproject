ea1b574ef39a3838a7129c5bcec1963437924a96
sugerhater <kehong0926@gmail.com>
2021-03-26 00:07:26 -0400

    rebuild home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState} from 'react';
 import Card from '../Card';
 import produce from '../produce.json';
 import ItemOption from '../ItemOption';
+import RapidApi from "../../utils/RapidApi";
 
 
 
-class Home extends Component {
+function Home() {
+  const [fruitSearch, setfruitSearch] = useState("");
+  const [fruitResult, setFruitResult] = useState(null);
 
-    state = {
-        produce
-    };
+  const handleInputChange = (event) =>{
+    const {value} = event.target;
+    setfruitSearch(value);
 
-    render() {
+    RapidApi.getIngredient(value, setFruitResult);
+}
+
         return (
             <div className="container mx-auto">
 
@@ -39,12 +44,14 @@ class Home extends Component {
 
                             <div className="col-md-5 col-lg-5 col-xl-5 form-group mx-auto">
                                 
-                                    <select className='form-control' id='all-produce'>
+                                    <select className='form-control' id='all-produce' onChange={handleInputChange}>
                                         <option id="0">All Produce</option>
-                                        {this.state.produce.map(item => (
+                                        {produce.map(item => (
                                             <ItemOption
                                                 id={item.name}
                                                 name={item.name}
+                                                value={item.name}
+                                                key={item.name}
                                             />
                                         ))}
                                             
@@ -60,14 +67,58 @@ class Home extends Component {
                 
                 <div className="row row-cols-1 row-cols-md-3">
 
-                    {this.state.produce.map(item => (
-                    <Card
-                            id={item.id}
-                            title={item.name}
-                            select={item.select}
-                            heart={item.heart}/>
-                    ))}
+                    
+                    
+                    {(fruitSearch.length) ? 
 
+                      
+                      
+                      (    <div className="col mb-4">
+                      <div className="card h-100 text-center">
+                        <span>
+                          <a href="#"
+                            className="badge badge-danger float-right">
+                            {/* <img src={process.env.PUBLIC_URL + props.heart} style={styles.heart} /> */}
+                          </a>
+                        </span>
+                
+                        <img
+                          src=
+                          className="card-img-top"
+                          alt={props.title} />
+                        <div className="card-body">
+                          <h5 className="card-title">{props.title}</h5>
+                          <p className="card-text">
+                            {props.select}
+                          </p>
+                
+                          <p className="card-text">
+                            <small className="text-muted">
+                              Nutritional Value: <br />
+                              {props.nutrition}
+                            </small></p>
+                          <div className="card-body">
+                            <a href="#" className="card-link">
+                              View Recipes
+                                                        </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>)
+                      
+                      :
+
+                      (produce.map(item => (
+                        <Card
+                        id={item.id}
+                        title={item.name}
+                        select={item.select}
+                        heart={item.heart}/>
+                        )) 
+                    )
+
+                        
+                      }
                    
 
                     
@@ -76,6 +127,4 @@ class Home extends Component {
             </div>
         )
     }
-}
-
 export default Home;
