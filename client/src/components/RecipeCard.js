125a56b681814142f22ff7df0b6ec02fdea60c69
sugerhater <kehong0926@gmail.com>
2021-04-02 01:54:58 -0400

    now it can show one recipe

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -12,7 +12,7 @@ const styles = {
 };
   
 
-const RecipeCard = (props) => {
+const RecipeCard = ({ recipe }) => {
  
     return (
         <div className="col mb-4">
@@ -27,16 +27,16 @@ const RecipeCard = (props) => {
           </span>
   
           <img
-            src='https://www.edamam.com/web-img/cbd/cbd028f1c51d03c3d9190ebfa19ced2f.jpg'
+            src={recipe.image}
             className="card-img-top"
-            alt={props.title} />
+            alt={recipe.label} />
           <div className="card-body">
   
             {
           // }<h5 className="card-title">{foodResult.food? foodResult.food.label:("")}</h5>
           }<h6 className="card-title">
             {/* <a rel="noreferrer noopener"href={"https://en.wikipedia.org/w/api.php?action=opensearch&search="+props.title+"&limit=1&format=json&origin=*"}>{props.title}</a> */}
-            Carrots With Caraway And Grapes
+            {recipe.label}
           </h6>
               
             <p className="card-text">
@@ -44,7 +44,11 @@ const RecipeCard = (props) => {
             </p>
               <div style={styles.option}>
             <p className="card-text" style={styles.option}>
-              <small className="text-muted" style={styles.option}><i>
+              <small className="text-muted" style={styles.option}>
+                {recipe.ingredientLines.map(ingredient =>{
+                  return <li>{ingredient}</li>
+                })}
+                {/* <i>
               6 x carrots(medium), peeled and 1/8-inch sliced on the diagonal<br/>
               4 tbsp Unsalted Butter <br/>
               1/2 tsp Caraway Seeds<br/>
@@ -53,9 +57,10 @@ const RecipeCard = (props) => {
               1/2 tsp Kosher Salt<br/>
               3 tbsp Light Brown Sugar<br/>
               3/4 cup grapes black or red seedless (halved)
-              </i></small></p></div>
+              </i> */}
+              </small></p></div>
             <div className="card-body">
-              <a href='http://www.lottieanddoof.com/2009/04/carrots-with-caraway-and-grapes/' className="card-link">
+              <a href={recipe.url} className="card-link">
                 View Full Recipe
                                           </a>
             </div>
--- client/src/components/pages/Recipe.js
+++ client/src/components/pages/Recipe.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 import recipeApi from '../../utils/recipeApi';
 import RecipeCard from '../RecipeCard';
+import { useStoreContext } from '../../store';
+import { Redirect } from "react-router-dom"
 
 
 const Recipe = (props) => {
-    const [recipes, setRecipes] = useState([]);
 
-   
-    
-    
-    return (
+
+    const [{ recipe } ] = useStoreContext();
+
+    return recipe ? (
         <div className="container">
 
             <div className="row row-cols-1 row-cols-md-3">
-                <RecipeCard />
-
-
+                <RecipeCard recipe={recipe} />
             </div>
 
         </div>
-    )
+    ) : <Redirect to={"/"} />
 }
 
 export default Recipe;
