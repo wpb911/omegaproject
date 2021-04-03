8057350a449f78429d6843866245301e50fa0fe3
Jacci Horvath <jaccinhorvath@gmail.com>
2021-04-03 10:53:47 -0400

    removing unneeded buttons

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -77,11 +77,11 @@ const LoginCard = () => {
           ? (
             <form className="form-signin" onSubmit={handleLoginSubmit} data-component="Form-Signin">
               <h1 className="h3 mb-3 font-weight-normal" style={{ textAlign: 'center' }}> Sign in </h1>
-              <div className="social-login">
+              {/* <div className="social-login">
                 <button className="btn facebook-btn social-btn text-center" type="button"><span><i className="fab fa-facebook-f" /> Sign in with Facebook </span></button>
                 <button className="btn google-btn social-btn text-center" type="button"><span><i className="fab fa-google" /> Sign in with Google </span></button>
-              </div>
-              <p style={{ textAlign: 'center' }}>OR</p>
+              </div> */}
+              {/* <p style={{ textAlign: 'center' }}>OR</p> */}
               <input type="email" onChange={(e) => setEmail(e.target.value)} id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
               <input type="password" onChange={(e) => setPassword(e.target.value)} id="inputPassword" className="form-control" placeholder="Password" required />
               <button className="btn btn-success btn-block" onSubmit={handleLoginSubmit} type="submit"><i className="fas fa-sign-in-alt" /> Sign in </button>
@@ -93,19 +93,19 @@ const LoginCard = () => {
           )
           : (
             <form action="/signup/" onSubmit={handleSignUpSubmit} className="form-signin">
-              <div className="social-login" style={{ textAlign: 'center' }}>
+              {/* <div className="social-login" style={{ textAlign: 'center' }}>
                 <button className="btn facebook-btn social-btn" type="button"><span><i className="fab fa-facebook-f" /> Sign up with Facebook </span></button>
               </div>
               <div className="social-login" style={{ textAlign: 'center' }}>
                 <button className="btn google-btn social-btn" type="button"><span><i className="fab fa-google" /> Sign up with Google </span></button>
               </div>
-              <p style={{ textAlign: 'center' }}>OR</p>
+              <p style={{ textAlign: 'center' }}>OR</p> */}
               <input type="text" onChange={(e) => setName(e.target.value)} id="user-name" className="form-control" placeholder="Full name" required autoFocus />
               <input type="email" onChange={(e) => setNewEmail(e.target.value)} id="user-email" className="form-control" placeholder="Email address" required autoFocus />
               <input type="password" onChange={(e) => setNewPassword(e.target.value)} id="user-pass" className="form-control" placeholder="Password" required autoFocus />
-              <input type="password" onChange={(e) => verifyNewPassword(e.target.value)} id="user-repeatpass" className="form-control" placeholder="Repeat Password" required autoFocus />
-              <button className="btn btn-primary btn-block" type="submit"><i className="fas fa-user-plus" /> Sign Up </button>
-              <button id="btn-back" className="fas fa-angle-left" type="button" onClick={onFormChange}>Back</button>
+              <input type="password" onChange={(e) => verifyNewPassword(e.target.value)} id="user-repeatpass" className="form-control" placeholder="Confirm Password" required autoFocus />
+              <button className="btn btn-primary btn-block" type="submit"><i className="fas fa-user-plus" /> Sign Up </button><br/>
+              <button id="btn-back" className="fas fa-angle-left btn-sm" type="button" onClick={onFormChange}>Back</button>
             </form>
           )
       }
