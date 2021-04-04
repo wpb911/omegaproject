e507e5493978a8252ff11774af5f7ee3636f858e
sugerhater <kehong0926@gmail.com>
2021-04-04 15:25:14 -0400

    add favorites into account page

--- client/src/components/pages/Account.js
+++ client/src/components/pages/Account.js
@@ -3,20 +3,35 @@ import FavoriteProduce from '../FavoriteProduce';
 import FavoriteRecipes from "../FavoriteRecipes";
 import Card from '../Card';
 import produce from '../produce.json';
+import { useAuthenticatedUser } from '../../utils/auth';
 
+function Account() {
+  const user = useAuthenticatedUser();
+  const userFavorite = produce.filter(fruit => {
+    if (user.favorites.includes(fruit.name)) {
+      return fruit
+    }
+  })
 
-class AccountFavorites extends Component {
-        state = {
-            produce
-        };
+  console.log(userFavorite)
 
-        render() {
-            return (
-                <div className="container mx-auto">
-                    <FavoriteProduce/>
-                    <FavoriteRecipes/>
-                </div>
-            )
-        }
+  return (
+      userFavorite.map(item =>(
+        <Card 
+        id={item.id}
+        title={item.name}
+        select={item.select}
+        image={item.image}
+        calories={item.calories}
+        fat={item.fat}
+        sugar={item.sugar}
+        cholesterol={item.cholesterol}
+        protein={item.protein}
+        serving={item.serving}
+        isFavorited={user && user.favorites.includes(item.name)}
+        season={item.season}/>
+      ))
+  )
+
 }
-export default AccountFavorites;
+export default Account;
